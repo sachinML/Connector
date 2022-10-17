f9feddf8847e13a93b33ce47e686e8cfffeca4ac
Sachin Jha <Sjha979@gmail.com>
2022-10-17 20:13:26 +0530

    updated the file

--- data_load.py
+++ data_load.py
@@ -5,4 +5,10 @@ from botocore.exceptions import ClientError
 import logging
 
 directory = "C:\\Users\\Visitor\\API\\tmp"
+try:
+    my_bucket = s3.Bucket('storeapidata')
+    my_bucket.objects.all().delete()
+    print("Objects from Bucket deleted Successfully.")
+except ClientError as e:
+    print(logging.error(e))
 
