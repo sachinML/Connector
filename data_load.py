f0c23cf816d150ca0161872bb341f958c666c490
Sachin Jha <Sjha979@gmail.com>
2022-10-19 21:08:19 +0530

    Update data_load.py

--- data_load.py
+++ data_load.py
@@ -13,4 +13,10 @@ try:
     print("Objects from Bucket deleted Successfully.")
 except ClientError as e:
     print(logging.error(e))
+try:
+    response = client.delete_bucket(Bucket='storeapidata')
+    print("Bucket Deleted Successfully")
+    print(json.dumps(response, indent=2))
+except ClientError as e:
+    print(logging.error(e))
 
